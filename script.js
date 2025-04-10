6adb7f3fd3604726476645116059df2265aef035
Lauraarodriguess <laura.rodriguess0563@gmail.com>
2025-04-10 10:58:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var pilotos = ["Verstappen","Leclerc","Sainz", "Albon", "antonelli", "Tsunoda", "Piastri", "Ocon" , "Bearman" , "Alonso", "Stroll", "Bortoleto", "Russell", "Hamilton","Norris","Senna"]
+var pilotoEscolhido = parseInt (prompt("Digite um número de 0 a 15"))
+
+document.write("Piloto escolhido: " + pilotos[pilotoEscolhido])
